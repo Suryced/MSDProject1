404788d773d6a33d736435a3f1085935e92b8a51
DonevinOdell <116240358+DonevinOdell@users.noreply.github.com>
2023-07-26 20:03:05 +0000

    Update Api.java

--- src/main/java/com/mcnz/events/Api.java
+++ src/main/java/com/mcnz/events/Api.java
@@ -1,7 +1,12 @@
-package com.mcnz.events;
+package Events;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +15,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 public class Api {
 
-//	@Autowired
-//	private ProjectDao projectDAO;
-//	
-//	@CrossOrigin
-//	@GetMapping("/api/events")
-//	public Collection<project> getAllProjects() {
-//		return projectDAO.getAllProjects();
-//	}
+	@Autowired
+	private ProjectDao projectDAO;
 	
 	@CrossOrigin
 	@GetMapping("/api/events")
-	public String getEvents() {
-		return "{\"id\":1,\"code\":\"1\",\"title\":\"All-Java Conference\",\"description\":\"Lectures and exhibits covering all Java topics\"}";
+	public Collection<project> getAllProjects() {
+		return projectDAO.getAllProjects();
+	}
+	
+//	@CrossOrigin
+//	@GetMapping("/api/events")
+//	public String getEvents() {
+//		return "{\"id\":1,\"code\":\"1\",\"title\":\"All-Java Conference\",\"description\":\"Lectures and exhibits covering all Java topics\"}";
+//	}
+	
+	@PutMapping("/api/events/update/{id}")
+	public String putEvents(@PathVariable String id) {
+		System.out.println(id);
+		return "Updated";
 	}
 	
 	
+	@DeleteMapping("/api/events/delete/{id}")
+	public String deleteEvents(@PathVariable String id) {
+		System.out.println(id);
+		return "Deleted";
+	}
+	
 }
+
