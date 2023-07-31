51a1cb0e41fce2525aca6622b102f4f92bcfb92b
DonevinOdell <116240358+DonevinOdell@users.noreply.github.com>
2023-07-31 20:08:18 +0000

    Update ProjectDao.java

--- src/main/java/com/mcnz/events/ProjectDao.java
+++ src/main/java/com/mcnz/events/ProjectDao.java
@@ -1,6 +1,5 @@
 package com.mcnz.events;
 
-
 import java.util.Collection;
 import java.util.List;
 
@@ -17,13 +16,13 @@ public class ProjectDao {
 	JdbcTemplate jdbcTemplate;
 	
 	public project saveEvent(project events) {
-		String sql = "INSERT INTO EVENT (EVENT_CODE, TITLE, DESCRIPTION) VALUES (?, ?, ?)";
+		String sql = "INSERT INTO EVENTS (EVENT_CODE, TITLE, DESCRIPTION) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, events.getEventCode(), events.getTitle(), events.getDescription());
         return events;
 	}
 	
 	public project updateEvent(project events, int event_id) {
-		String sql = "UPDATE EVENT SET EVENT_CODE=?, TITLE=?, DESCRIPTION=? WHERE ID = ?";
+		String sql = "UPDATE EVENTS SET EVENT_CODE=?, TITLE=?, DESCRIPTION=? WHERE ID = ?";
 
         jdbcTemplate.update(sql, events.getEventCode(), events.getTitle(), events.getDescription(), event_id);
         return events;
@@ -31,18 +30,18 @@ public class ProjectDao {
 	}
 	
 	public Collection<project> getAllEvents() {
-		return jdbcTemplate.query("Select * from EVENT", new
+		return jdbcTemplate.query("Select * from EVENTS", new
 				BeanPropertyRowMapper<project>(project.class));
 	}
 	
 	public project getEvent(int id) {
-		String sql = "SELECT * FROM EVENT WHERE id = ?";
+		String sql = "SELECT * FROM EVENTS WHERE id = ?";
 		List<project> events = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(project.class), id);
 		return events.isEmpty() ? null : events.get(0);
 	}
 	
 	public void deleteEvent(int id) throws Exception {
-		String sql = "DELETE FROM EVENT WHERE id = ?";
+		String sql = "DELETE FROM EVENTS WHERE id = ?";
 		try {
 			jdbcTemplate.update(sql, id);
 		}catch(DataAccessException e) {
