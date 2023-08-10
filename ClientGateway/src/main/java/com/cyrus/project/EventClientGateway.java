6170850c9f5a36edfd526afd3cca4dcea6252f60
Me <me@me.com>
2023-08-10 11:54:38 +0000

    stuff

--- ClientGateway/src/main/java/com/cyrus/project/EventClientGateway.java
+++ ClientGateway/src/main/java/com/cyrus/project/EventClientGateway.java
@@ -2,6 +2,7 @@ package com.cyrus.project;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+@CrossOrigin
 @RestController
 public class EventClientGateway {
 	
--- authentication-service/src/main/java/com/mcnz/project/TokenApi.java
+++ authentication-service/src/main/java/com/mcnz/project/TokenApi.java
@@ -2,11 +2,13 @@ package com.mcnz.project;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/account")
 public class TokenApi {
