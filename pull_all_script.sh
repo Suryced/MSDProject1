b0af7fdd0cefdfed2a805dced6efbeab41115fe6
cyrus <wang_cyrus@bah.com>
2023-08-14 18:44:56 +0000

    pull images script

---
+++ pull_all_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker pull bobthebbqmaster/authentication-service
+docker pull bobthebbqmaster/client-gateway-service
+docker pull bobthebbqmaster/registrations-service
+docker pull bobthebbqmaster/events-service
+docker pull bobthebbqmaster/customers-service
